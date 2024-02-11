c8bd3abec0f6124390afacbc2f0030ae4de7f567
Henry Field <=>
2024-02-11 19:35:49 +0000

    Initial commit

---
+++ Normal Airhorn/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using LCSoundTool;
+using UnityEngine;
+
+namespace Normal_Airhorn
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            AudioClip newSound;
+            newSound = SoundTool.GetAudioClip("Meowpixel-Normal Airhorn", "airhorn.mp3");
+
+            SoundTool.ReplaceAudioClip("AirHorn1", newSound);
+
+        }
+    }
+}
---
+++ Normal Airhorn/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Normal_Airhorn
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "Normal_Airhorn";
+        public const string PLUGIN_NAME = "Normal_Airhorn";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
